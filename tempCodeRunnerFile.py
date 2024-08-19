ec833ebcb1c70b011e846c219d0b807f4b08ffdb
Shehroze-Safdar1 <shehroze.safdar@gmail.com>
2024-08-19 17:10:47 +0500

    Added features related to leveling up

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -107,6 +107,7 @@ class AlienInvasion:
             #Destroy existing bullets and create the new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
   
     def _create_fleet(self):
         '''Create the fleet of the alien'''
@@ -188,6 +189,8 @@ class AlienInvasion:
         '''Start a new game when the player click the play button'''
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            #Reset the game speed to initial speed.
+            self.settings.initialize_dynamic_settings()
             # Reset the game statistics.
             self.stats.reset_stats()
             self.game_active = True
